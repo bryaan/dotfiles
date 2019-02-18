ca7b9dc0c5f0de0b880d128d62eb8086384ed05c
Bryan <mail@bryaan.com>
2019-02-18 03:30:49 -0500

    bug fix on clean-brew.sh

--- macos/clean-brew.sh
+++ macos/clean-brew.sh
@@ -3,7 +3,9 @@ brew update
 brew upgrade
 # Keep only programs with links, and only the last linked version.
 brew cleanup -s
-brew cask cleanup
+brew cask upgrade
 # diagnotics
 brew doctor
 brew missing
+brew cask doctor
+
