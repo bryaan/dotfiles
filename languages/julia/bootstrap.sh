424ad2ad7bd291e2b1ad30db7b5189100848c2f5
Bryan Rivera <mail@bryaan.com>
2019-05-01 19:20:56 -0400

    small updates
    - add doc for julia on windows
    - add remap for undo from ctrl-y to ctrl-z

--- languages/julia/bootstrap.sh
+++ languages/julia/bootstrap.sh
@@ -1,6 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -sf $DIR/config/juno_startup.jl $HOME/.julia/config/
-ln -sf $DIR/config/startup_ijulia.jl $HOME/.julia/config/
 ln -sf $DIR/config/startup.jl $HOME/.julia/config/
-
+ln -sf $DIR/config/startup_ijulia.jl $HOME/.julia/config/
+ln -sf $DIR/config/juno_startup.jl $HOME/.julia/config/
